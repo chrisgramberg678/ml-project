da3fc18647dd932bff7b084ec5aafa14fa8b11c3
Chris Gramberg <chrisgramberg678@gmail.com>
2017-03-06 17:37:53 -0600

    rewrote gradient descent as a c++ class

--- gradient_descent.cpp
+++ gradient_descent.cpp
@@ -1,40 +1,22 @@
-/*First impementation of gradient descent*/
+/*Impementation of gradient_descent*/
 
-#include <iostream>
-#include <Eigen/Dense>
-#include <math.h>
-#include <vector>
-using namespace std;
-using Eigen::Vector2d;
-
-vector<int> x = {1,2,3,4};
-vector<int> y = {6,5,7,10};
-
-// computes a new vector ab based on the summation of the derivatives with 
-// x and y subbed in from the training data
-Vector2d dL(Vector2d ab){
-	double da = 15*ab(0) + 5*ab(1) - 38.5;
-	double db = 2*ab(1) + 5*ab(0) - 14;
-	Vector2d temp;
-	temp << da,db;
-	// cout << "tempDL: " << endl << temp << endl;
-	return temp;
-}
+#include "gradient_descent.h"
 
 // dL/da = 1/N sum(-2x(y-xa-b))
 // where x and y come from the training data and N is the size of x and y
-double dLda(double a, double b){
+
+double gradient_descent::dLda(double a, double b){
 	double sum = 0;
 	for(int i = 0; i < x.size(); ++i){
 		sum += (-2)*(x[i])*(y[i]-((a*x[i])+b));
 	}
 	sum /= x.size();
-	return sum;
+	return sum;	
 }
 
 // dL/db = 1/N sum(-2(y-ax-b))
 // where x and y come from the training data and N is the size of x and y
-double dLdb(double a, double b){
+double gradient_descent::dLdb(double a, double b){
 	double sum = 0;
 	for(int i = 0; i < x.size(); ++i){
 		sum += (-2)*(y[i]-((a*x[i])+b));
@@ -44,7 +26,7 @@ double dLdb(double a, double b){
 }
 
 // computes a new vector by doing the summation over the training data 
-Vector2d dLs(Vector2d ab){
+Vector2d gradient_descent::dL(Vector2d ab){
 	double a = dLda(ab(0),ab(1));
 	double b = dLdb(ab(0),ab(1));
 	Vector2d temp;
@@ -54,7 +36,7 @@ Vector2d dLs(Vector2d ab){
 
 // L(a,b) = 1/N sum (y-ax-b)^2
 // we know this is working when the value of the loss function gets smaller
-double Loss(double a, double b){
+double gradient_descent::Loss(double a, double b){
 	double sum = 0;
 	for(int i = 0; i < x.size(); ++i){
 		double temp = y[i] - ((a*x[i]) + b);
@@ -64,26 +46,42 @@ double Loss(double a, double b){
 	return sum;
 }
 
-int main(){
-// step size
-double gamma = .001;
-// when |x_old - x_new| > precision we're there
-double precision = .000000001;
-Vector2d x_old, x_new, diff;
-// these don't matter b/c they are trashed once we get into the while
-x_old << 10,10;
-// starting a and b will be (0,0)
-x_new << 0,0;
-diff = x_old - x_new;
-double loss = 10000000;
-int i = 0;
-while((fabs(diff(0)) > precision || fabs(diff(1)) > precision)){
-    cout << "iteration: " << i++ << endl;
-	cout << "Loss " << loss << endl;
-	x_old = x_new;
-	x_new += -gamma*dL(x_old);
+gradient_descent::gradient_descent(vector<int> n, vector<int> m):
+	x(n),
+	y(m)
+	{}
+	
+// does the actual fitting using gradient descent
+// params: 
+Vector2d gradient_descent::fit(Vector2d ab, double gamma, double precision, bool verbose){
+	Vector2d x_old, x_new, diff;
+	// these don't matter b/c they are trashed once we get into the while
+	x_old << ab(0)+1, ab(1)+1;
+	// starting a and b will be (0,0)
+	x_new = ab;
 	diff = x_old - x_new;
-	loss = Loss(x_new(0),x_new(1));
+	double loss = 10000000;
+	int i = 0;
+	while((fabs(diff(0)) > precision || fabs(diff(1)) > precision)){
+   		cout << "iteration: " << i++ << endl;
+		cout << "Loss " << loss << endl;
+		x_old = x_new;
+		x_new += -gamma*dL(x_old);
+		diff = x_old - x_new;
+		loss = Loss(x_new(0),x_new(1));
+	}
+	cout << "coefficients: " << endl << x_new << endl;
+	return x_new;
 }
-cout << "coefficients: " << endl << x_new << endl;
+
+int main(){
+	double gamma = .001;
+	double precision = .000000001;
+	vector<int> x = {1,2,3,4};
+	vector<int> y = {6,5,7,10};
+	Vector2d ab;
+	ab << 0,0;
+	gradient_descent gd(x,y);
+	Vector2d res = gd.fit(ab,gamma,precision,true);
+	cout << "result " << endl << res << endl;
 }
---
+++ gradient_descent.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <math.h> // used for fabs() -- absolute value of float
+#include <vector>
+using namespace std;
+using Eigen::Vector2d;
+
+class gradient_descent{
+	private:
+		// the data we're going to fit a line to
+		vector<int> x,y;
+
+		// dL/da = 1/N sum(-2x(y-xa-b))
+		// where x and y come from the training data and N is the size of x and y
+		double dLda(double a, double b);
+
+		// dL/db = 1/N sum(-2(y-ax-b))
+		// where x and y come from the training data and N is the size of x and y
+		double dLdb(double a, double b);
+
+		// computes a new vector by doing the summation over the training data 
+		Vector2d dL(Vector2d ab);
+
+		// L(a,b) = 1/N sum (y-ax-b)^2
+		// we know this is working when the value of the loss function gets smaller
+		double Loss(double a, double b);
+	public:
+		// construction uses to vectors to represent data
+		gradient_descent(vector<int> n, vector<int> m);
+
+		// does the actual work of fitting a function to the data
+		Vector2d fit(Vector2d ab, double gamma, double precision, bool verbose = false);
+};
