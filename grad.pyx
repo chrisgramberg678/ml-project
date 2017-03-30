be130ad64ef3f0eea5eebc3f54b2cb3044befe33
Chris Gramberg <chrisgramberg678@gmail.com>
2017-03-30 18:16:10 -0500

    updated Cython interface to work with new implementation of gradient_descent

--- grad.pyx
+++ grad.pyx
@@ -5,24 +5,26 @@ import numpy as np
 cdef extern from "gradient_descent.h" :
 	cdef cppclass gradient_descent:
 		gradient_descent() except + 
-		gradient_descent(vector[int], vector[int]) except +
-		vector[double] py_fit(vector[int], double, double, int) except +
+		gradient_descent(vector[ vector[double] ], vector[double]) except +
+		vector[double] py_fit(vector[double], double, double) except +
 
 cdef class PyGradient_Descent:
 	cdef gradient_descent c_gd	# the C++ object that does gradient_descent
 	def __cinit__(self, x, y):
 		# convert the lists x and y into vectors that we can use
-		cdef vector[int] _x = list(x)
-		cdef vector[int] _y = list(y)
+		cdef vector[ vector [double] ] _x = list(list(x))
+		cdef vector[double] _y = list(y)
 		self.c_gd = gradient_descent(_x, _y)
-	def fit(self, ab, double gamma, double precision):
-		# convert the vector[double] to a numpy array
-		cdef vector[int] _ab = ab
-		cdef vector[double] _res = self.c_gd.py_fit(_ab, gamma, precision, 0)
+	def fit(self, init, double gamma, double precision):
+		# convert the initial values to a vector of doubles
+		cdef vector[double] _init = init
+		# then call the function and convert the resulting
+		# vector[double] to a numpy array
+		cdef vector[double] _res = self.c_gd.py_fit(_init, gamma, precision)
 		return np.array(list(_res))
 # simple test 
 def test():
-	xs = np.array([1,2,3,4])
-	ys = np.array([6,5,7,10])
+	xs = np.array([[7, 5, 3, 5], [6, 2, 9, 1]])
+	ys = np.array([50.32, 37.97, 12.2, 39.69])
 	gd = PyGradient_Descent(xs,ys)
 	print gd.fit([0,0],.001,.000000001)
