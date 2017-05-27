5792af4465ea31a8dc00e875456d759a5d5e29b7
chris <chrisgramberg678@gmail.com>
2017-05-27 23:37:35 +0000

    fixed an issue where an eigen assert was failing

--- model.cpp
+++ model.cpp
@@ -12,7 +12,7 @@ double model::loss(VectorXd w, MatrixXd X, VectorXd y){return 0;}
 linear_least_squares_model::linear_least_squares_model(){}
 
 VectorXd linear_least_squares_model::gradient(VectorXd w, MatrixXd X, VectorXd y){
-	VectorXd result;
+	VectorXd result(w.rows());
 	for(int i = 0; i < X.cols(); ++i){
 		result += 2*(X.col(i) * ((w.transpose() * X.col(i)) - y(i)))/X.cols();
 	}
@@ -38,7 +38,7 @@ binary_logistic_regression_model::binary_logistic_regression_model(){}
 
 // sum(i=1->N){x_i * (Pr(y=1|x_i) - y_1)}
 VectorXd binary_logistic_regression_model::gradient(VectorXd w, MatrixXd X, VectorXd y){
-	VectorXd result;
+	VectorXd result(w.rows());
 	for(int i = 0; i < X.cols(); ++i){
 		result += (( exp(w.transpose() * X.col(i)) / (1 + exp(w.transpose() * X.col(i)))) - y(i)) * X.col(i);
 	}
@@ -53,3 +53,4 @@ double binary_logistic_regression_model::loss(VectorXd w, MatrixXd X, VectorXd y
 	}
 	return loss;
 }
+
